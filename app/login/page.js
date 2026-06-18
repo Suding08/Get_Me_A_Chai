bf6f5ffec221828120bff0078f61149011abe849
Suding08 <gsudin52@gmail.com>
2026-06-18 23:50:50 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
 
