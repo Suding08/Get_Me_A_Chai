8e76f09b512193b98442ed02a5f6c2dbadfc35b7
Suding08 <gsudin52@gmail.com>
2026-06-18 22:45:29 +0530

    Implement Razorpay payment verification route

---
+++ app/api/razorpay/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils";
+import Payment from "@/models/Payment";
+import connectDB from "@/db/connectDb";
+import Razorpay from "razorpay";
+import User from "@/models/User";
+
+export const POST = async (req) => {
+    await connectDB()
+    let body = await req.formData()
+    body = Object.fromEntries(body)
+
+    //check if razorpayOrderId is present on the server
+    let p = await Payment.findOne({oid: body.razorpay_order_id})
+    if (!p){
+        return NextResponse.json({success: false, message:"Order ID not found"})
+    }
+
+    //fetch the secret of the user who is getting the payment
+    let user = await User.findOne({username: p.to_user})
+    const secret = user.razorpaysecret
+
+    //verify the payment signature
+    let xx = validatePaymentVerification({"order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id},body.razorpay_signature, secret)
+
+    if(xx){
+        //update payment status
+        const updatedPayment = await Payment.findOneAndUpdate({oid: body.razorpay_order_id},{done: "true"},{new: true})
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_user}?paymentdone = true`)
+    }
+    else{
+        return NextResponse.json({success: false, message:"Payment verification failed"})
+    }
+}
