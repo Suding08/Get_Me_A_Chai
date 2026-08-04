51aab1ee51d92c05e1d867bcfac82ddc1f32045c
Suding08 <gsudin52@gmail.com>
2026-08-04 14:07:14 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link'
 import { useState } from "react"
+import Search from './Search'
 
 function Navbar() {
   const { data: session } = useSession()
@@ -59,6 +60,7 @@ function Navbar() {
 
         {session && <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer" onClick={() => { signOut() }}>Logout</button>}
 
+{!session &&  <Search />}
        {!session && <Link href={"/"}>
        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer">Home</button>
         </Link>}
