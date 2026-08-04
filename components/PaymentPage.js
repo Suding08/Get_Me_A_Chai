50f2ec0a1cd103ca0a23866bb827a9cf619c4a4a
Suding08 <gsudin52@gmail.com>
2026-08-04 14:21:04 +0530

    Update PaymentPage.js

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -62,7 +62,7 @@ const PaymentPage = ({ username }) => {
             "key": currentUser.razorpayid, // Enter the Key ID generated from the Dashboard
             "amount": amount, // Amount is in currency subunits. 
             "currency": "INR",
-            "name": "Buy Me A Chai", //your business name
+            "name": "Get Me A Chai", //your business name
             "description": "Test Transaction",
             "image": "https://example.com/your_logo",
             "order_id": orderId, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
